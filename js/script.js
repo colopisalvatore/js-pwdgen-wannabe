45c3d769040b7a0033b15c864a2ba173c38023b1
colopisalvatore <salvatorecolopi1989@gmail.com>
2022-10-05 15:56:03 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -7,9 +7,13 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21(dove il 21 è in questo 
 */
 
 const userName = prompt ('Il tuo Nome?');
+// console.log('userName', userName)
 const userSurname = prompt ('Ed ora il tuo Cognome?');
+// console.log('userSurname', userSurname)
 const userColor = prompt ('Il tuo colore preferito?');
+// console.log('userColor', userColor)
 let userAge = parseInt(prompt('Inserisci la tua età'));
+// console.log('userAge', userAge)
 
 const result = `
 Benvenuto ${userName}!
@@ -17,7 +21,9 @@ Benvenuto ${userName}!
 La tua password generata è: ${userName}${userSurname}${userColor}${userAge}
 
 `
+// console.log('result', result)
 
 const element = document.getElementById('myId');
+// console.log('element', element)
 
 element.innerHTML = result;
